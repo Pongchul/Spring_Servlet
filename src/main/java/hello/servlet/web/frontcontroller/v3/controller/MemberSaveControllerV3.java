bba177dbd58bd1a0c3a0f9c7292cc48339f5c2e7
pongchul <inchul7511@gmail.com>
2023-02-03 22:05:15 -0800

    V3 -> V2 서블릿 종속성 제거(모델 뷰 추가), 뷰 이름 중복 제

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,28 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));        // Map<String, String> 이렇게 들어왔는데 age는 int! 이것을 parseInt로 파싱해야 한다.
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
+
+        return mv;
+    }
+}
